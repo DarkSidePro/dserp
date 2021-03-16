4ce8408779fe6ad523b541541cb2d9a91e7df2fd
Patryk Tchorzewski <patryk@dark-side.pro>
2021-03-16 19:01:56 +0100

    finish admin member

--- src/Controller/AdminMemberController.php
+++ src/Controller/AdminMemberController.php
@@ -31,6 +31,7 @@ class AdminMemberController extends AbstractController
     public function index(Request $request): Response
     {
         $this->denyAccessUnlessGranted('ROLE_SUPER_ADMIN');
+
         $table = $this->datatableFactory->create([])
         ->add('id', TextColumn::class, ['label' => '#', 'className' => 'bold', 'searchable' => true])
         ->add('firstname', TextColumn::class, ['label' => 'Firstname', 'className' => 'bold', 'searchable' => true])
@@ -39,7 +40,7 @@ class AdminMemberController extends AbstractController
         ->add('registred', DateTimeColumn::class, ['label' => 'Registred', 'className' => 'bold', 'searchable' => true, 'format' => 'Y-m-d'])
         ->add('actions', TwigColumn::class, [
             'className' => 'id',
-            'template' => 'admin_member/_partials/actions.html.twig',
+            'template' => 'admin_member/_partials/table/actions.html.twig',
             'label' => 'Actions',
             'searchable' => false
 
@@ -54,8 +55,6 @@ class AdminMemberController extends AbstractController
             return $table->getResponse();
         }  
 
-
-
         return $this->render('admin_member/index.html.twig', [
             'controller_name' => 'AdminMemberController',
             'datatable' => $table
@@ -63,7 +62,7 @@ class AdminMemberController extends AbstractController
     }
 
     /**
-     * @Route("/admin/member/{id}", name="admin_member_update")
+     * @Route("/admin/member/update/{id}", name="admin_member_update")
      */
     public function update(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder, User $user): Response
     {
@@ -87,7 +86,7 @@ class AdminMemberController extends AbstractController
             } elseif ($role == 1) {
                 $user->setRoles(['ROLE_ADMIN', 'ROLE_USER']);
             } elseif ($role == 2) {
-                $user->setRoles(['ROLE_SUPER_ADMIN', 'ROLE_ADMIN', 'ROLE_USER']);
+                $user->setRoles(array('ROLE_SUPER_ADMIN'));
             }
 
             $user->setRegistred(new \DateTime);
@@ -102,7 +101,7 @@ class AdminMemberController extends AbstractController
     }
 
     /**
-     * @Route("/admin/member-new", name="admin_member_add")
+     * @Route("/admin/member/new", name="admin_member_add")
      */
     public function add(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder): Response
     {
@@ -121,13 +120,14 @@ class AdminMemberController extends AbstractController
             );
 
             $role = $form->get('role')->getData();
+            $role = $role['0'];
 
             if ($role == 0) {
                 $user->setRoles(['ROLE_USER']);
             } elseif ($role == 1) {
                 $user->setRoles(['ROLE_ADMIN', 'ROLE_USER']);
             } elseif ($role == 2) {
-                $user->setRoles(['ROLE_SUPER_ADMIN', 'ROLE_ADMIN', 'ROLE_USER']);
+                $user->setRoles(array('ROLE_SUPER_ADMIN'));
             }
 
             $user->setRegistred(new \DateTime);
@@ -138,14 +138,14 @@ class AdminMemberController extends AbstractController
             return $this->redirectToRoute('admin_member');
         }
 
-        return $this->render('admin_member/update.html.twig', [
+        return $this->render('admin_member/new/index.html.twig', [
             'controller_name' => 'AdminController',
             'form' => $form->createView()
         ]);
     }
 
     /**
-     * @Route("/admin/member/delete/{id}", name="admin_member_delete")
+     * @Route("/admin/member/delete/{id}", name="admin_member_remove")
      */
     public function deleteUser(User $user, EntityManagerInterface $em): Response
     {
@@ -155,5 +155,48 @@ class AdminMemberController extends AbstractController
         $em->flush();
         
         return $this->redirectToRoute('admin_member');
+    }
+
+    /**
+     * @Route("/admin/member/update/{id}", name="admin_member_update")
+     */
+    public function updateMember(
+            User $user, 
+            EntityManagerInterface $em, 
+            Request $request, 
+            UserPasswordEncoderInterface $passwordEncoder
+        ): Response
+    {
+        $form = $this->createForm(AdminUserType::class, $user);
+        $form->handleRequest($request);
+ 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $role = $form->get('role')->getData();
+            $role = $role['0'];
+
+            if ($role == 0) {
+                $user->setRoles(['ROLE_USER']);
+            } elseif ($role == 1) {
+                $user->setRoles(['ROLE_ADMIN', 'ROLE_USER']);
+            } elseif ($role == 2) {
+                $user->setRoles(array('ROLE_SUPER_ADMIN'));
+            }
+
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectToRoute('admin_member_update', ['id' => $user->getId()]);
+        }
+
+        return $this->render('admin_member/new/update.html.twig', [
+            'controller_name' => 'AdminController',
+            'form' => $form->createView()
+        ]);
     }
 }
