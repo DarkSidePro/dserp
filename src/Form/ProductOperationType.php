d2468283c6de397002618e27ac4594013b7d9659
Patryk Tchorzewski <patryk@dark-side.pro>
2021-03-18 13:45:13 +0100

    remove form fields

--- src/Form/ProductOperationType.php
+++ src/Form/ProductOperationType.php
@@ -8,6 +8,7 @@ use App\Entity\ProductOperation;
 use App\Entity\Shipment;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,19 +18,7 @@ class ProductOperationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('enter')
-            ->add('dispatch')
-            ->add('modification')
-            ->add('production')
-            ->add('state')
-            ->add('production_id', EntityType::class, [
-                'class' => Production::class,
-                'choice_label' => 'id'
-            ])
-            ->add('shipment_id', EntityType::class, [
-                'class' => Shipment::class,
-                'choice_label' => 'id'
-            ])
+            ->add('modification', NumberType::class)
             ->add('save', SubmitType::class, [
                 'attr' => ['class' => 'btn btn-success']
             ])
